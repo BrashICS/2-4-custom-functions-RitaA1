fbea35cf477c8628244cf3a92566213e738c04c8
Rita <rita.anierobi@stu.ocsb.ca>
2024-10-10 14:49:57 +0000

    2

--- main.js
+++ main.js
@@ -7,7 +7,11 @@
  * This is a playground. Feel free to play.
  **/ 
 
+    
+let volume = 5
 
+
+/** 
 // Example custom function using Number() and prompt()
 function circle_area() {
 
@@ -20,6 +24,7 @@ function circle_area() {
 /*****  Your code goes below  *****/
 
 //Part 1
+/** 
 function to_fahrenheit() {
  let celsius = +prompt("write a temperature in celsius:")
  let fahrenheit = Math.round((celsius * 9/5) + 32)
@@ -51,3 +56,43 @@ function roll_die6() {
     }
     
 roll_die6()
+
+**/
+
+//practice
+function top_value() {
+let value = +prompt("What is the top number:")
+value = Math.round(Math.random()*value + 1)
+console.log(`Number is: ${value}`)
+}
+
+
+function show_volume() {
+console.log(volume)
+}
+
+function volume_up() {
+volume = volume + 1
+console.log(show_volume())
+}
+
+function volume_down() {
+    volume = volume - 1
+    console.log(show_volume())
+}
+
+function random_volume(){
+    let volume = Math.round(Math.random()*10 + 1)
+    console.log(volume)
+}
+
+function born_in() {
+    let age = +prompt("How old are you?")
+    let year = 2024 - age
+    console.log(`You were born in ${year}`)
+}
+
+function roll_d6() {
+    console.log(Math.floor(Math.random()*6 + 1))
+
+}
